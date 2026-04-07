37c72d6e9b6e798d934d70a12a4e245eee1e02e3
cyan soong <paxonzephyr@gmail.com>
2026-04-08 01:39:28 +0800

    feat:USART接收发送消息

---
+++ Hardware/USART_TX_RX.c
@@ -0,0 +1,144 @@
+#include "stdarg.h"
+#include "stdio.h"
+#include "stm32f10x.h" // Device headerc
+
+void Seria_TX_RX_Init(void)
+{
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    USART_InitTypeDef USART_InitSructure;
+    USART_InitSructure.USART_BaudRate = 9600;
+    USART_InitSructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None; // 硬件流使能:否,不使用
+    USART_InitSructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;                 // 接收,发送使能
+    USART_InitSructure.USART_Parity = USART_Parity_No;                             // 奇偶校验
+    USART_InitSructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitSructure.USART_StopBits = USART_StopBits_1;
+    USART_Init(USART1, &USART_InitSructure);
+
+    // 使用中断读取
+    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+    NVIC_InitTypeDef NVIC_InitStructure;
+    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    USART_Cmd(USART1, ENABLE);
+    USART_ClearFlag(USART1, USART_FLAG_TC); // TC标志位在初始化后是置1的情况
+}
+
+void Serial_SendByte(uint8_t Byte)
+{
+    USART_SendData(USART1, Byte);
+    while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET)
+        ;
+    // 关于清除标志位,由于对USART->DR进行写操作时会将TXE标志位清零,所以不用再手动(软件)清除
+}
+
+void Serial_SendArray(uint8_t Array[], uint16_t length)
+{
+    for (uint16_t i = 0; i < length; i++)
+    {
+        Serial_SendByte(Array[i]);
+    }
+}
+
+void Seria_SendString(char *String)
+{
+    while (*String)
+    {
+        Serial_SendByte(*String);
+        String++;
+    }
+}
+
+void Seria_SendNumber(uint32_t number)
+{
+
+    uint32_t num = number;
+    uint32_t mask = 1;
+    while (num > 9)
+    {
+        num /= 10;
+        ;
+        mask *= 10;
+    }
+
+    while (mask)
+    {
+        Serial_SendByte((uint8_t)(number / mask) + '0');
+        number %= mask;
+        mask /= 10;
+    }
+}
+
+// //重定向printf函数
+// int fputc(int ch ,FILE* f)
+// {
+//     Serial_SendByte(ch);
+//     return ch;
+// }
+
+int fputc(int ch, FILE *f) // 对printf函数进行了重定义
+{
+
+    USART_SendData(USART1, (uint8_t)ch); // 串口1,发送一个数据
+    while (RESET == USART_GetFlagStatus(USART1, USART_FLAG_TC))
+        ; // 等待发送完成
+    return ch;
+}
+
+void Serial_Printf(char *format, ...)
+{
+    char String[100];
+    va_list arg;
+    va_start(arg, format);
+    vsprintf(String, format, arg);
+    va_end(arg);
+    Seria_SendString(String);
+}
+
+volatile uint8_t Serial_RxData;
+volatile uint8_t Serial_RxFlag;
+
+// 读后自动清除
+uint8_t Serial_GetRXFlag(void)
+{
+    if (Serial_RxFlag == 1)
+    {
+        Serial_RxFlag = 0;
+        return 1;
+    }
+    return 0;
+}
+
+// 读数据
+uint8_t Serial_GetRxData(void)
+{
+    return Serial_RxData;
+}
+
+void USART1_IRQHandler(void)
+{
+    if (USART_GetITStatus(USART1, USART_IT_RXNE) == SET)
+    {
+        Serial_RxData = USART_ReceiveData(USART1);
+        Serial_RxFlag = 1;
+        USART_ClearITPendingBit(USART1, USART_IT_RXNE);
+    }
+}
---
+++ Hardware/USART_TX_RX.h
@@ -0,0 +1,18 @@
+#ifndef __USART_TX_RX_H
+#define __USART_TX_RX_H
+#include "stm32f10x.h"
+
+void Seria_TX_RX_Init(void);
+void Serial_SendByte(uint8_t Byte);
+void Serial_SendArray(uint8_t Array[],uint16_t length);
+void Seria_SendString(char* String);
+void Seria_SendNumber(uint32_t number);
+void Serial_Printf(char* format,...);
+
+uint8_t Serial_GetRxData(void);
+uint8_t Serial_GetRXFlag(void);
+
+
+
+#endif
+
