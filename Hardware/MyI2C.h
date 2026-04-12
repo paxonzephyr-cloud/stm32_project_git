bad51709642bcf8983a071b1b03f5c4a3c94881f
cyan soong <paxonzephyr@gmail.com>
2026-04-12 15:01:45 +0800

    feat:软件I2C的基本时序和设备地址应答

---
+++ Hardware/MyI2C.h
@@ -0,0 +1,29 @@
+#ifndef __MYI2C_H
+#define __MYI2C_H
+#include "stm32f10x.h" // Device header
+#include "Delay.h"
+
+typedef enum{
+    PULL,
+    RELEASE,
+}state;
+
+typedef enum{
+    LOW,
+    HIGH,
+}state2;
+
+
+void MyI2C_Init(void);
+void MyI2C_Start(void);
+void MyI2C_Stop(void);
+void MyI2C_SendByte(uint8_t Byte);
+uint8_t MyI2C_ReceiveByte(void);
+void MyI2C_SendAck(uint8_t AckBit);
+uint8_t MyI2C_ReceiveAck(void);
+
+void MyI2C_W_SCL(uint8_t BitValue);
+void MyI2C_W_SDA(uint8_t BitValue);
+uint8_t MyI2C_R_SDA(void);
+
+#endif
---
+++ examples/I2C_soft_test.c
@@ -0,0 +1,21 @@
+#include "Delay.h"
+#include "stm32f10x.h" // Device header
+#include "OLED.h"
+#include "MyI2C.h"
+
+int main(void)
+{
+    OLED_Init();
+    MyI2C_Init();
+
+    MyI2C_Start();
+    MyI2C_SendByte(0xD0);
+    uint8_t Ack=MyI2C_ReceiveAck();
+    MyI2C_Stop();
+
+    OLED_ShowNum(1,1,Ack,3);
+
+    while (1)
+    {
+    }
+}
