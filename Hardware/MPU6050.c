4773573a5d837a71b64ac23ffc3a81ef02223193
cyan soong <paxonzephyr@gmail.com>
2026-04-12 16:11:01 +0800

    feat:使能传感器并读取传感器数

--- Hardware/MPU6050.c
+++ Hardware/MPU6050.c
@@ -6,6 +6,49 @@
 void MPU6050_Init(void)
 {
     MyI2C_Init();
+    MPU6050_WriteReg(MPU6050_PWR_MGMT_1,0x01);//唤醒从机---可以写入
+    MPU6050_WriteReg(MPU6050_PWR_MGMT_2,0x00);//2-不循环,6-每个轴不待机
+    MPU6050_WriteReg(MPU6050_SMPLRT_DIV,0x09);//10分频
+    MPU6050_WriteReg(MPU6050_CONFIG,0x06);//-2,3-外部同步(不需要),3-滤波(设置为110)
+
+    MPU6050_WriteReg(MPU6050_GYRO_CONFIG,0x18);//3-自检,2-量程选择(11-最大),-3无关位
+    MPU6050_WriteReg(MPU6050_ACCEL_CONFIG,0x18);//3-自检,2-量程选择(11-最大),3-高通滤波器
+}
+
+void MPU6050_GetDatad(int16_t* AccX,int16_t* AccY,int16_t* AccZ,
+                        int16_t* GyroX,int16_t* GyroY,int16_t* GyroZ)
+{
+    uint16_t DataH,DataL;
+    DataH=MPU6050_ReadReg(MPU6050_ACCEL_XOUT_H);
+    DataL=MPU6050_ReadReg(MPU6050_ACCEL_XOUT_L);
+    *AccX=(DataH<<8)|DataL;//这是一个用补码表示的有符号数,可以直接给指针
+    
+    DataH=MPU6050_ReadReg(MPU6050_ACCEL_YOUT_H);
+    DataL=MPU6050_ReadReg(MPU6050_ACCEL_YOUT_L);
+    *AccY=(DataH<<8)|DataL;
+
+    DataH=MPU6050_ReadReg(MPU6050_ACCEL_ZOUT_H);
+    DataL=MPU6050_ReadReg(MPU6050_ACCEL_ZOUT_L);
+    *AccZ=(DataH<<8)|DataL;
+
+    DataH=MPU6050_ReadReg(MPU6050_GYRO_XOUT_H);
+    DataL=MPU6050_ReadReg(MPU6050_GYRO_XOUT_L);
+    *GyroX=(DataH<<8)|DataL;
+
+    DataH=MPU6050_ReadReg(MPU6050_GYRO_YOUT_H);
+    DataL=MPU6050_ReadReg(MPU6050_GYRO_YOUT_L);
+    *GyroY=(DataH<<8)|DataL;    
+    
+    DataH=MPU6050_ReadReg(MPU6050_GYRO_ZOUT_H);
+    DataL=MPU6050_ReadReg(MPU6050_GYRO_ZOUT_L);
+    *GyroZ=(DataH<<8)|DataL;
+
+    //还可以通过指针自增,一次性读取所有寄存器的值
+}
+
+uint8_t MPU6050_GetID(void)
+{
+    return MPU6050_ReadReg(MPU6050_WHO_AM_I);
 
 }
 
--- examples/I2C_soft_test.c
+++ examples/I2C_soft_test.c
@@ -3,22 +3,23 @@
 #include "OLED.h"
 #include "MPU6050.h"
 
+int16_t AX,AY,AZ,GX,GY,GZ;
+
 int main(void)
 {
     OLED_Init();
     MPU6050_Init();
-
-    MPU6050_WriteReg(0x6B,0x00);//解除睡眠模式
-    MPU6050_WriteReg(0x19,0x66);
-
-
-    uint8_t Psc=MPU6050_ReadReg(0x19);//写入预分频
-    OLED_ShowHexNum(1,1,Psc,4);
-    
-    uint8_t ID=MPU6050_ReadReg(0x75);//读取硬件ID号----6500<-->70
-    OLED_ShowHexNum(2,1,ID,4);
-
-    while (1)
-    {
+    uint8_t ID=MPU6050_GetID();
+    OLED_ShowString(1,1,"ID:");
+    OLED_ShowHexNum(1,4,ID,2);
+    while (1){
+        MPU6050_GetDatad(&AX,&AY,&AZ,&GX,&GY,&GZ);
+        OLED_ShowSignedNum(2,1,AX,5);
+        OLED_ShowSignedNum(3,1,AY,5);
+        OLED_ShowSignedNum(4,1,AZ,5);
+        
+        OLED_ShowSignedNum(2,8,GX,5);
+        OLED_ShowSignedNum(3,8,GY,5);
+        OLED_ShowSignedNum(4,8,GZ,5);
     }
 }
